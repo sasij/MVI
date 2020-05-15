c14089f30859621b640294c4a710ab15745523b8
Juanjo <juanjose.sanchez@domestika.org>
2020-05-15 20:13:24 +0200

    Make State immutable. Save State on rotations.

--- app/src/main/java/com/juanjo/demo/store/Store.kt
+++ app/src/main/java/com/juanjo/demo/store/Store.kt
@@ -6,36 +6,45 @@ import com.juanjo.demo.state.State
 import com.juanjo.demo.utils.Logger
 import io.reactivex.Scheduler
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import io.reactivex.subjects.BehaviorSubject
 import io.reactivex.subjects.PublishSubject
 
 class Store(
-  private var state: State = State(),
+  private val state: State = State(),
   private val logger: Logger = Logger(),
   private val uiScheduler: Scheduler = AndroidSchedulers.mainThread(),
   private val ioScheduler: Scheduler = Schedulers.computation()
 ) {
   private var inPipeline: PublishSubject<Action> = PublishSubject.create()
   private var outPipeline: BehaviorSubject<State> = BehaviorSubject.create()
+  private var disposable: Disposable? = null
 
-  fun getState() = state
+  init {
+    mainPipe()
+  }
 
   fun getDispatch(action: Action) {
     inPipeline.onNext(action)
   }
 
   //the name should be "subscribe" but...it is horrible when we use on the view
-  fun changes() = inPipeline
-    .switchMap { it.execute() }
-    .scan(state) { state, action -> Reducer.reduce(state, action) }
-    .distinctUntilChanged()
-    .doOnNext {
-      state = it
-      //TODO push to fabric
-      logger.log(it.toString())
-    }
-    .mergeWith(outPipeline)
-    .subscribeOn(ioScheduler)
-    .observeOn(uiScheduler)
+  fun changes() = outPipeline.hide()
+
+  private fun mainPipe() {
+    disposable = inPipeline
+      .switchMap { it.execute() }
+      .scan(state) { state, action -> Reducer.reduce(state, action) }
+      .distinctUntilChanged()
+      .doOnNext {
+        //TODO save states in a list for debugging
+        //state = it
+        //TODO push to fabric
+        logger.log(it.toString())
+      }
+      .subscribeOn(ioScheduler)
+      .observeOn(uiScheduler)
+      .subscribe { outPipeline.onNext(it) }
+  }
 }
